f74c0382ec0078769fef4da62c492445b6188660
VIJAY PATIDAR <vramotiya@gmail.com>
2020-07-13 13:25:16 +0530

    HomeFragment implemented

--- app/src/main/java/com/vkpapps/thunder/ui/fragments/HomeFragment.kt
+++ app/src/main/java/com/vkpapps/thunder/ui/fragments/HomeFragment.kt
@@ -3,9 +3,13 @@ package com.vkpapps.thunder.ui.fragments
 import android.content.Context
 import android.os.Bundle
 import android.view.*
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.navigation.NavDirections
+import androidx.navigation.Navigation
 import com.vkpapps.thunder.R
 import com.vkpapps.thunder.interfaces.OnNavigationVisibilityListener
+import kotlinx.android.synthetic.main.fragment_home.*
 
 /***
  * @author VIJAY PATIDAR
@@ -21,6 +25,26 @@ class HomeFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         setHasOptionsMenu(true)
+
+        val controller = Navigation.findNavController(view)
+        photo.setOnClickListener {
+            controller.navigate(getDestination(0))
+        }
+        audio.setOnClickListener {
+            controller.navigate(getDestination(1))
+        }
+        video.setOnClickListener {
+            controller.navigate(getDestination(2))
+        }
+        files.setOnClickListener {
+            controller.navigate(getDestination(3))
+        }
+        internal.setOnClickListener {
+            Toast.makeText(requireContext(), "Not Implemented Yet ", Toast.LENGTH_SHORT).show()
+        }
+        external.setOnClickListener {
+            Toast.makeText(requireContext(), "Not Implemented Yet ", Toast.LENGTH_SHORT).show()
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
@@ -37,6 +61,21 @@ class HomeFragment : Fragment() {
         super.onAttach(context)
         if (context is OnNavigationVisibilityListener) {
             onNavigationVisibilityListener = context
+        }
+    }
+
+    private fun getDestination(des: Int): NavDirections {
+        return object : NavDirections {
+            override fun getArguments(): Bundle {
+                val bundle = Bundle()
+                bundle.putInt(GenericFragment.PARAM_DESTINATION, des)
+                return bundle
+            }
+
+            override fun getActionId(): Int {
+                return R.id.action_navigation_home_to_navigation_generic
+            }
+
         }
     }
 }
